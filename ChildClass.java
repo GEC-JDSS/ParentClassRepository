9ad4f119a00fa806396bc85874cb208ccbeca661
MargaretLGreen <mgreen@hfcc.edu>
2017-01-10 10:01:37 -0500

    Add files via upload

---
+++ ChildClass.java
@@ -0,0 +1,16 @@
+/**
+ * Child Class has grade field
+ * @author greenml
+ *
+ */
+public class ChildClass extends ParentClass {
+	private int grade;  	// holds grade of child
+
+	public int getGrade() {
+		return grade;
+	}
+
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+}
---
+++ ParentClass.java
@@ -0,0 +1,25 @@
+/**
+ * Parent Class for adding to with GitHub
+ * @author greenml
+ *
+ */
+public class ParentClass {
+	private String name; // holds name of parent object
+	private int age;	 // holds age of parent object
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+
+}
