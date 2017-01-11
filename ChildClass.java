413953fb1d1fb867936eed4bd212c7fdfbf6cde6
MargaretLGreen <mgreen@hfcc.edu>
2017-01-11 10:42:20 -0500

    Added SchoolName field

--- ChildClass.java
+++ ChildClass.java
@@ -5,6 +5,9 @@
  */
 public class ChildClass extends ParentClass {
 	private int grade;  	// holds grade of child
+	private String schoolName;
+	
+	
 
 	public int getGrade() {
 		return grade;
@@ -12,5 +15,13 @@ public class ChildClass extends ParentClass {
 
 	public void setGrade(int grade) {
 		this.grade = grade;
+	}
+
+	public String getSchoolName() {
+		return schoolName;
+	}
+
+	public void setSchoolName(String schoolName) {
+		this.schoolName = schoolName;
 	}
 }
